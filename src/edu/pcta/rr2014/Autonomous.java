bcfa8b226387e7bfdb594411b9121d6e7d8d0ea2
pmurphy29 <patrick.home@cox.net>
2014-03-05 07:06:52 -0500

    Update Autonomous.java
    Auto has been updated. Note the superStructure.fire() command has been added to SuperStructure class.

--- src/edu/pcta/rr2014/Autonomous.java
+++ src/edu/pcta/rr2014/Autonomous.java
@@ -13,10 +13,30 @@ import edu.pcta.rr2014.systems.SuperStructure;
  */
 public class Autonomous {
     
-    public static void run(DriveTrain driveTrain, SuperStructure superStructure){
-        
+      public static void run(DriveTrain driveTrain, SuperStructure superStructure, boolean delayed){
+        driveTrain.drive(.35, .35);
+        try{
+            Thread.sleep(3000);
+        }
+        catch(InterruptedException ex){
+            System.out.println("Auto sleep interrupted");
+        }
+        if(delayed){
+            try{
+                System.out.println("4 second delay");
+                Thread.sleep(4000);
+            }
+            catch(InterruptedException ex){
+             System.out.println("Auto delay interrupted");   
+            }
+        }
+        driveTrain.drive(0,0);
+        superStructure.fire();
         
         
     }
+    
+}
+
     
 }
